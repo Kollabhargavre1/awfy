ed6400856ed109515e8865a455ed210d58cdef66
kollabhargavre1 <kollabhargavreddy@gmail.com>
2024-06-06 22:54:15 +0530

    Setting up repository for the first time

---
+++ src/main/java/com/backend/awfy/spring/AwfyApplication.java
@@ -0,0 +1,13 @@
+package com.backend.awfy.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/awfy/spring/AwfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.awfy.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
